d82a631cb10fe26010c7d539589b6e4dd5e0bd1a
Daniel Shaw <danielshaw71@gmail.com>
2024-05-18 15:56:30 +0100

    Finished hero section

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,16 +1,25 @@
 import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/hero-img.png';
-import themeIcon from '../../assets/sun-black.svg';
-import githubIcon from '../../assets/github-black.svg';
-import linkedinIcon from '../../assets/linkedin-black.svg';
+import sun from '../../assets/sun-black.svg';
+import moon from '../../assets/moon-white.svg';
+import githubBlack from '../../assets/github-black.svg';
+import githubWhite from '../../assets/github-white.svg';
+import linkedinBlack from '../../assets/linkedin-black.svg';
+import linkedinWhite from '../../assets/linkedin-white.svg';
 import CV from '../../assets/cv.pdf';
+import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
+  const { theme, toggleTheme } = useTheme();
+  const themeIcon = theme === 'light' ? sun : moon;
+  const githubIcon = theme === 'light' ? githubBlack : githubWhite;
+  const linkedinIcon = theme === 'light' ? linkedinBlack : linkedinWhite;
+
   return (
     <section id ="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
         <img className={styles.hero} src={heroImg} alt="Avatar profile picture" />
-        <img className={styles.colorMode} src={themeIcon} alt="Color mode icon" />
+        <img className={styles.colorMode} src={themeIcon} alt="Color mode icon" onClick={toggleTheme} />
       </div>
       <div className={styles.info}>
         <h1>
@@ -27,8 +36,8 @@ function Hero() {
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
-        <p>With a passion for both frontend and backend development.</p>
-        <a href={CV} download='cv.pdf'>
+        <p className={styles.description}>With a passion for both frontend and backend development.</p>
+        <a href={CV} download>
           <button className="hover">CV</button>
         </a>
       </div>
