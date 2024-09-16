8cd3120463dc577b9288918fabbbbff28e3d3b84
Daniel Shaw <danielshaw71@gmail.com>
2024-09-16 12:21:29 +0100

    Updated/added new projects

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import './AnimatedTitle.css'
 
-function ProjectCard({ src, link, h3, p }) {
+function ProjectCard({ src, link, h3, p, hasAnimation }) {
   return (
     <a href={link} target="_blank">
-      <img className="hover" src={src} alt={`${h3} logo`} />
+      {hasAnimation ? (
+        <div className="animatedTitle hover">
+          <h1>{h3}</h1>
+        </div>
+      ) : (
+        <img className="hover" src={src} alt={`${h3} logo`} />
+      )}
       <h3>{h3}</h3>
       <p>{p}</p>
     </a>
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,4 +1,5 @@
 import styles from './ProjectsStyles.module.css';
+import phi from '../../assets/phi-3.5-law-logo.png';
 import insite from '../../assets/insite-logo.png';
 import vloyal from '../../assets/vloyal-logo.png';
 import supperclub from '../../assets/supperclub-logo.png';
@@ -9,6 +10,8 @@ function Projects() {
     <section id ="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
+        <ProjectCard src={phi} link="https://github.com/DanielShaw98/data-prep" h3="Phi-3.5-Law" p="Trained Phi-3.5 model, with ability to retrieve specific clauses from contracts" />
+        <ProjectCard link="https://github.com/DanielShaw98/dev-up" h3="DevUp" p="Connecting developers and founders to collaborate on projects" hasAnimation={true} />
         <ProjectCard src={insite} link="https://github.com/DanielShaw98/insite" h3="Insite" p="E-learning/content marketplace" />
         <ProjectCard src={vloyal} link="https://github.com/DanielShaw98/VLoyal" h3="Vloyal" p="Virtual loyalty card wallet" />
         <ProjectCard src={supperclub} link="https://github.com/DanielShaw98/SupperClub" h3="SupperClub" p="Airbnb clone for supperclub events" />
